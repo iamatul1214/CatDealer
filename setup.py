bd8a1a5b56b9381c583b542bbf82c16c1a90f652
atulkumarrai <iamatul1214@gmail.com>
2021-12-12 15:52:48 +0530

    directory change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as file:
 
 setup(
    name='CatDealer',
-   version='2.0.0',
+   version='2.1.0',
    description='A library which can deal with Categorical values in a dataframe',
    license="MIT",
    long_description=long_description,
