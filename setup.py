7b622e7495c36dfac78892c0c83169fda8b3edbb
atulkumarrai <iamatul1214@gmail.com>
2021-12-12 17:12:36 +0530

    trying to deploy

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md','r') as file:
 
 setup(
    name='CatDealer',
-   version='2.1.0',
+   version='2.1.2',
    description='A library which can deal with Categorical values in a dataframe',
    license="MIT",
    long_description=long_description,
    author='Atul Rai',
    author_email='iamatul1214@gmail.com',
    url="https://github.com/iamatul1214/CatDealer",
-    packages=['Cat_Dealer'],
+    packages=['CatDealer'],
     #No requirement of external packages as dependencies
     classifiers=['Programming Language :: Python :: 3.8',
                  'Intended Audience :: Developers',
